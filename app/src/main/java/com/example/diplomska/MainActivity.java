805cbfdfe800b28984d30b7f8e1ed3f4e70be56d
watzo95 <marko.watzak@hotmail.com>
2020-06-15 14:49:25 +0200

    Bluetooth conn

--- app/src/main/java/com/example/diplomska/MainActivity.java
+++ app/src/main/java/com/example/diplomska/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.diplomska;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cardiomood.android.controls.gauge.SpeedometerGauge;
 import com.sccomponents.gauges.library.ScArcGauge;
@@ -14,12 +21,67 @@ import com.sccomponents.gauges.library.ScNotches;
 import com.sccomponents.gauges.library.ScPointer;
 import com.sccomponents.gauges.library.ScWriter;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
     // Find the components
+    private BluetoothDevice device;
+    private BluetoothAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList deviceStrs = new ArrayList();
+        final ArrayList devices = new ArrayList();
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0)
+        {
+            for (BluetoothDevice device : pairedDevices)
+            {
+                deviceStrs.add(device.getName() + "\n" + device.getAddress());
+                devices.add(device.getAddress());
+            }
+        }
+
+        // show list
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.select_dialog_singlechoice,
+                deviceStrs.toArray(new String[deviceStrs.size()]));
+
+        alertDialog.setSingleChoiceItems(adapter, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+                int position = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+                String deviceAddress = (String) devices.get(position);
+
+                BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                BluetoothDevice device = btAdapter.getRemoteDevice(deviceAddress);
+
+                UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+                try {
+                    BluetoothSocket socket = null;
+                    socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+                    socket.connect();
+                } catch (IOException e) {
+                    Toast.makeText(getBaseContext(),"Do sem sem prišel", Toast.LENGTH_SHORT);
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        alertDialog.setTitle("Izberite napravo Bluetooth:");
+        alertDialog.show();
     }
 }
